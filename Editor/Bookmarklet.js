efc1aa3840a4b9cf0df7c92ddce6d0ffd88252a7
Roben Kleene <robenk@gmail.com>
2012-06-19 13:36:57 -0400

    Copy update

--- Editor/Bookmarklet.js
+++ Editor/Bookmarklet.js
@@ -1,13 +1,13 @@
 // javascript:var%20regularExpression%20=%20/((&|\?)q=)([^&]*)(?!.*&q=)/g;var%20match%20=%20regularExpression.exec(window.location);var%20searchString%20=%20match[3];searchString%20=%20decodeURIComponent(searchString);searchString%20=%20searchString.replace(/\+site:.*$/,%20%27%27);searchString%20=%20searchString.replace(/\%22/g,%20%27%27);searchString%20=%20%22\%22%22%20+%20searchString%20+%20%22\%22%22;siteSearches%20=%20%22+site:nytimes.com+OR+site:newyorker.com%22;searchString%20=%20searchString%20+%20siteSearches;window.location%20=%20%22http://www.google.com/search?q=%22%20+%20searchString;
 
-// Searches The New York Times and The New Yorker websites for the phrase just searched for in Google. The idea being you can find cases of usage on those sites to determine if you're using the phrase correctly. I.e., it's a tool for copy editing.
+// To assist in copy editing, it takes a phrase just searched for in Google and narrows the search to just the websites for The New York Times and The New Yorker. The idea being, if those sites are doing it, then it's probably edited correctly.
 
 // Usage:
-// 1. While write a phrase in a document you are working on, you aren't sure if you're punctuation or usage is correct. 
-// 2. Copy the phrase you are unsure of to the clipboard.
-// 3. Perform a google search with that phrase.
-// 4. Trigger this bookmarklet, the bookmarklet will narrow that search to just the most authoritatively edited publications.
-// 5. If you see your usage in the search results, you can probably assume it's correct.
+// 1. While authoring a document, you aren't sure if you're punctuating or using a phrase correctly. 
+// 2. Copy that phrase to the clipboard.
+// 3. Perform a google search with that phrase. (You have to actually hit the search button, or otherwise perform the search, before triggering the bookmarklet.)
+// 4. Trigger this bookmarklet, your search will be narrowed to just well-edited publications.
+// 5. If your usage is in the search results, then it's probably correct.
 
 // Grab the Search String
 var regularExpression = /((&|\?)q=)([^&]*)(?!.*&q=)/g;
