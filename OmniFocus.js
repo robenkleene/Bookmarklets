208a1016865b5b2885ee33725fa9c090c6fbab67
Roben Kleene <robenk@gmail.com>
2012-10-29 01:01:21 -0400

    Updated Bookmarklets to work with iOS 6

--- OmniFocus.js
+++ OmniFocus.js
@@ -1,4 +1,4 @@
-// javascript:var%20linkTitle%20=%20encodeURIComponent(document.title);var%20linkLocation%20=%20encodeURIComponent(window.location);var%20newLocation%20=%20%27omnifocus:///add?name=%27+linkTitle+%27&note=%27+linkLocation;var%20selectedText%20=%20%27%27;if%20(window.getSelection)%20{selectedText%20=%20window.getSelection();}%20else%20if%20(document.getSelection)%20{selectedText%20=%20document.getSelection();}%20else%20if%20(document.selection)%20{selectedText%20=%20document.selection.createRange().text;}if%20(selectedText%20!=%20%27%27)%20{newLocation%20=%20newLocation%20+%20escape(%22\n\n%22)%20+%20selectedText;}window.location%20=%20newLocation;
+// javascript:var%20linkTitle%20=%20encodeURIComponent(document.title);var%20linkLocation%20=%20encodeURIComponent(window.location);var%20newLocation%20=%20%27omnifocus:///add?name=%27+linkTitle+%27&note=%27+linkLocation;var%20selectedText%20=%20%27%27;if%20(window.getSelection)%20{selectedText%20=%20window.getSelection();}%20else%20if%20(document.getSelection)%20{selectedText%20=%20document.getSelection();}%20else%20if%20(document.selection)%20{selectedText%20=%20document.selection.createRange().text;}if%20(selectedText%20!=%20%27%27)%20{newLocation%20=%20newLocation%20+%20escape(%22\n\n%22)%20+%20encodeURIComponent(selectedText);}window.location%20=%20newLocation;
 
 // OmniFocus bookmarklet that adds the current web page as a new action in OmniFocus. This bookmarklet extends the official OmniFocus bookmarklet by adding the currently selected text to the action's note.
 
@@ -19,7 +19,7 @@ if (window.getSelection) {
 }
 // Add the selectedText to the new location
 if (selectedText != '') {
-	newLocation = newLocation + escape("\n\n") + selectedText;
+	newLocation = newLocation + escape("\n\n") + encodeURIComponent(selectedText);
 }
 
 window.location = newLocation;
--- Pastebot Markdown Link.js
+++ Pastebot Markdown Link.js
@@ -1,4 +1,4 @@
-// javascript:var%20linkTitle%20=%20encodeURIComponent(%22[%22%20+%20document.title%20+%20%22]%22);var%20linkLocation%20=%20encodeURIComponent(%22(%22%20+%20window.location%20+%20%22)%22);var%20newLocation%20=%20%27pastebot://%27+linkTitle+linkLocation;window.location%20=%20newLocation;
+// javascript:var%20linkTitle%20=%20encodeURIComponent(%22[%22%20+%20document.title%20+%20%22]%22);var%20linkLocation%20=%20encodeURIComponent(%22(%22%20+%20window.location%20+%20%22)%22);var%20newLocation%20=%20%27pastebot://%27+linkTitle+linkLocation;var%20selectedText%20=%20%27%27;if%20(window.getSelection)%20{selectedText%20=%20window.getSelection();}%20else%20if%20(document.getSelection)%20{selectedText%20=%20document.getSelection();}%20else%20if%20(document.selection)%20{selectedText%20=%20document.selection.createRange().text;}if%20(selectedText%20!=%20%27%27)%20{selectedText%20=%20selectedText.toString();selectedText%20=%20selectedText.replace(/^/gm,%20%22>%20%22);newLocation%20=%20newLocation%20+%20encodeURIComponent(%22:%22)%20+%20escape(%22\n\n%22)%20+%20encodeURIComponent(selectedText);}window.location%20=%20newLocation;
 
 // Sends a markdown URL to the current web page to Pastebot, which automatically copies it to the clipboard. I.e., this is a shortcut to copy a markdown link to the current web page to the clipboard.
 
@@ -23,7 +23,7 @@ if (window.getSelection) {
 if (selectedText != '') {
 	selectedText = selectedText.toString();
 	selectedText = selectedText.replace(/^/gm, "> ");
-	newLocation = newLocation + ":" + escape("\n\n") + selectedText;
+	newLocation = newLocation + encodeURIComponent(":") + escape("\n\n") + encodeURIComponent(selectedText);
 }
 
 window.location = newLocation;
