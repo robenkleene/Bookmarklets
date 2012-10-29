5934e065aa2a4a6c5866494051ee5c6513833d47
Roben Kleene <robenk@gmail.com>
2012-10-29 00:36:48 -0400

    Adding quoting selection to markdown link bookmarklet

--- Pastebot Markdown Link.js
+++ Pastebot Markdown Link.js
@@ -7,7 +7,23 @@
 // Get the link
 var linkTitle = encodeURIComponent("[" + document.title + "]");
 var linkLocation = encodeURIComponent("(" + window.location + ")");
-
 var newLocation = 'pastebot://'+linkTitle+linkLocation;
+
+// Get the selection
+// Note: This method of getting the selected text works on Safari for Mac and  on iPad as long as the bookmark is in the bookmarks bar. It does not work on iPhone.
+var selectedText = '';
+if (window.getSelection) {
+	selectedText = window.getSelection();
+} else if (document.getSelection) {
+	selectedText = document.getSelection();
+} else if (document.selection) {
+	selectedText = document.selection.createRange().text;
+}
+// Add the selectedText to the new location
+if (selectedText != '') {
+	selectedText = selectedText.toString();
+	selectedText = selectedText.replace(/^/gm, "> ");
+	newLocation = newLocation + ":" + escape("\n\n") + selectedText;
+}
 
 window.location = newLocation;
