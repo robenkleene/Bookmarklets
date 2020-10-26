a0493572a8c3e64fcb9964f95ce0eef8867dfcb6
Roben Kleene <contact@robenkleene.com>
2020-10-26 14:22:33 -0400

    Add search

---
+++ Search.js
@@ -0,0 +1,34 @@
+var initialLocation = window.location.href;
+var duckDuckGoPrefix = "https://duckduckgo.com/";
+var googlePrefix = "https://www.google.com/search";
+var googleImageParameter = "&tbm=isch";
+var duckImageParameter = "&ia=images&iax=images";
+var findString;
+var replaceString;
+var secondFindString;
+var secondReplaceString;
+if (initialLocation.lastIndexOf(duckDuckGoPrefix, 0) === 0) {
+  findString = duckDuckGoPrefix;
+  replaceString = googlePrefix;
+  secondFindString = duckImageParameter;
+  secondReplaceString = googleImageParameter;
+} else if (initialLocation.lastIndexOf(googlePrefix, 0) === 0) {
+  findString = googlePrefix;
+  replaceString = duckDuckGoPrefix;
+  secondFindString = googleImageParameter;
+  secondReplaceString = duckImageParameter;
+} else {
+  var regularExpression = /\/([^\s\/]+)/g;
+  var match = regularExpression.exec(window.location);
+  var searchString = match[1];
+  window.location =
+    "http://www.duckduckgo.com/?q=site:" +
+    searchString +
+    encodeURIComponent(" ");
+}
+if (findString) {
+  var newLocation = initialLocation
+    .replace(findString, replaceString)
+    .replace(secondFindString, secondReplaceString);
+  window.location = newLocation;
+}
